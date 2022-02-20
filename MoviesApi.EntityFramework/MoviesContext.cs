5f40d546af2c2311fcc703d5a827d70d4c1dced2
dchiriac <8166892+MrGodlike6@users.noreply.github.com>
2022-02-20 18:13:34 +0200

    Add Movie DbSet.

--- MoviesApi.EntityFramework/MoviesContext.cs
+++ MoviesApi.EntityFramework/MoviesContext.cs
@@ -1,11 +1,16 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using MoviesApi.Domain.Entities;
 
 namespace MoviesApi.EntityFramework
 {
     public class MoviesContext : DbContext
     {
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
         public MoviesContext(DbContextOptions<MoviesContext> options)
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
             : base(options)
         { }
+
+        public DbSet<Movie> Movies { get; set; }
     }
 }
