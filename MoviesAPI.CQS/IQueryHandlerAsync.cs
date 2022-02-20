0161032869ffbf9cd4f041a0700a9cb612916475
dchiriac <8166892+MrGodlike6@users.noreply.github.com>
2022-02-20 18:50:48 +0200

    Remove unused usings.

--- MoviesAPI.CQS/IQueryHandlerAsync.cs
+++ MoviesAPI.CQS/IQueryHandlerAsync.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MoviesAPI.CQS
+﻿namespace MoviesAPI.CQS
 {
     public interface IQueryHandlerAsync<TQuery, TResult> where TQuery : IQuery<TResult>
     {
