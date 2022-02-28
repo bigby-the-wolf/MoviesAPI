6f038d06cc4085965b1926405fc6209cf94216b0
dchiriac <8166892+MrGodlike6@users.noreply.github.com>
2022-02-28 19:51:26 +0200

    Add logging for command handlers.

--- MoviesAPI/Utilities/DependencyInjection/CQSRegistration.cs
+++ MoviesAPI/Utilities/DependencyInjection/CQSRegistration.cs
@@ -1,4 +1,6 @@
-﻿using MoviesApi.Domain.Commands;
+﻿using MoviesApi.Domain.CommandHandlers;
+using MoviesApi.Domain.Commands;
+using MoviesApi.EntityFramework;
 using MoviesApi.EntityFramework.CommandHandlers;
 using MoviesAPI.CQS;
 
@@ -8,7 +10,14 @@ namespace MoviesAPI.Utilities.DependencyInjection
     {
         internal static void RegisterCQS(IServiceCollection services)
         {
-            services.AddScoped<ICommandHandlerAsync<CreateMovieCommand>, CreateMovieCommandHandlerAsync>();
+            services.AddScoped<ICommandHandlerAsync<CreateMovieCommand>>(s =>
+            {
+                var logger = s.GetRequiredService<ILogger<LoggingCommandHandlerAsyncDecorator<CreateMovieCommand>>>();
+                var context = s.GetRequiredService<MoviesContext>();
+
+                var createMovieCommandHandlerAsync = new CreateMovieCommandHandlerAsync(context);
+                return new LoggingCommandHandlerAsyncDecorator<CreateMovieCommand>(logger, createMovieCommandHandlerAsync);
+            });
         }
     }
 }
