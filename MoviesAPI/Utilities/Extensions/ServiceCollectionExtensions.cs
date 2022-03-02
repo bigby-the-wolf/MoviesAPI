166fe2533d7d1c62181eaa1df633263e8d35f7be
dchiriac <8166892+MrGodlike6@users.noreply.github.com>
2022-03-02 18:19:50 +0200

    Move DI registration to extension methods.

---
+++ MoviesAPI/Utilities/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Data.SqlClient;
+using MoviesApi.Domain.CommandHandlerDecorators;
+using MoviesApi.Domain.Commands;
+using MoviesApi.EntityFramework;
+using MoviesApi.EntityFramework.CommandHandlers;
+using MoviesAPI.CQS;
+using Polly;
+using Polly.Registry;
+
+namespace MoviesAPI.Utilities.Extensions
+{
+    internal static class ServiceCollectionExtensions
+    {
+        internal static void RegisterCQS(this IServiceCollection services)
+        {
+            services.AddScoped<ICommandHandlerAsync<CreateMovieCommand>>(s =>
+            {
+                var logger = s.GetRequiredService<ILogger<LoggingCommandHandlerAsyncDecorator<CreateMovieCommand>>>();
+                var context = s.GetRequiredService<MoviesContext>();
+
+                var createMovieCommandHandlerAsync = new CreateMovieCommandHandlerAsync(context);
+                return new LoggingCommandHandlerAsyncDecorator<CreateMovieCommand>(logger, createMovieCommandHandlerAsync);
+            });
+        }
+
+        internal static void ConfigurePollyPolicies(this IServiceCollection services)
+        {
+            var registry = new PolicyRegistry
+            {
+                { PollyPolicies.SqlResiliencePolicy, Policy.Handle<SqlException>().CircuitBreakerAsync(2, TimeSpan.FromMinutes(1)) }
+            };
+
+            services.AddSingleton<IReadOnlyPolicyRegistry<string>>(registry);
+        }
+    }
+}
