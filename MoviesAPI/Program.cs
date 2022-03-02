f7f78e6584f2825315386c5607926578eb57998c
dchiriac <8166892+MrGodlike6@users.noreply.github.com>
2022-03-02 19:46:47 +0200

    Add exception filter for circuit breaker.

--- MoviesAPI/Program.cs
+++ MoviesAPI/Program.cs
@@ -9,6 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers(options =>
 {
     options.Filters.Add<NotImplementedExceptionFilter>();
+    options.Filters.Add<BrokenCircuitExceptionFilter>();
 });
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
