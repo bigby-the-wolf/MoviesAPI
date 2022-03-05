a0283eee770c160717cad7779ab5d6b356072fc5
dchiriac <8166892+MrGodlike6@users.noreply.github.com>
2022-03-05 16:54:54 +0200

    Add get all movies query handler and tests.

---
+++ MoviesApi.EntityFramework.Tests/QueryHandlers/GetAllMoviesQueryHandlerAsyncTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MoviesApi.Domain.Queries;
+using MoviesApi.EntityFramework.QueryHandlers;
+using MoviesApi.EntityFramework.Tests.Fixtures;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MoviesApi.EntityFramework.Tests.QueryHandlers
+{
+    public class GetAllMoviesQueryHandlerAsyncTests  : IClassFixture<TestDatabaseFixture>
+    {
+        public GetAllMoviesQueryHandlerAsyncTests(TestDatabaseFixture dbFixture)
+            => DbFixture = dbFixture;
+
+        public TestDatabaseFixture DbFixture { get; }
+
+        [Fact]
+        public async Task HandleGetAllMovies()
+        {
+            using var context = DbFixture.CreateContext();
+            var sut = new GetAllMoviesQueryHandlerAsync(context);
+            var getAllMoviesQuery = new GetAllMoviesQuery();
+
+            var movies = await sut.HandleAsync(getAllMoviesQuery);
+
+            var moviesInDb = await context.Movies.ToListAsync();
+            Assert.False(moviesInDb.Except(movies).Any(), "Must get exactly all movies from DB.");
+        }
+    }
+}
