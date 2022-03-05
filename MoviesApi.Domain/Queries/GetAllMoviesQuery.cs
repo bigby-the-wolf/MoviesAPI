44c056921d439d21681991b61e9b99a8230daa61
dchiriac <8166892+MrGodlike6@users.noreply.github.com>
2022-03-05 15:48:19 +0200

    Add get all movies query.

---
+++ MoviesApi.Domain/Queries/GetAllMoviesQuery.cs
@@ -0,0 +1,9 @@
+﻿using MoviesApi.Domain.Entities;
+using MoviesAPI.CQS;
+
+namespace MoviesApi.Domain.Queries
+{
+    public class GetAllMoviesQuery : IQuery<IReadOnlyCollection<Movie>>
+    {
+    }
+}
